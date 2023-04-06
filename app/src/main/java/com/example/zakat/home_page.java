ade2f0226b3247d992a9e8d0ab222eb25dd3b692
rameem2003 <rameem2019@gmail.com>
2023-04-06 22:44:02 +0600

    bug fixed

--- app/src/main/java/com/example/zakat/home_page.java
+++ app/src/main/java/com/example/zakat/home_page.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class home_page extends AppCompatActivity {
-    private CardView personal;
+    private CardView personal, organization, rules, about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,38 @@ public class home_page extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         personal = findViewById(R.id.personal);
+        organization = findViewById(R.id.organizartion);
+        rules = findViewById(R.id.rules);
+        about = findViewById(R.id.about);
 
         personal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(home_page.this, personal_zakat.class);
+                startActivity(i);
+            }
+        });
+
+        organization.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(home_page.this, organizational_zakat.class);
+                startActivity(i);
+            }
+        });
+
+        rules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(home_page.this, rules.class);
+                startActivity(i);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(home_page.this, about.class);
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/com/example/zakat/organizational_zakat.java
@@ -0,0 +1,14 @@
+package com.example.zakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class organizational_zakat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizational_zakat);
+    }
+}
