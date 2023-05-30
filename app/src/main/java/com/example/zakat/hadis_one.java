1e44ca59620a74ef19230a3523193587534608c7
rameem2003 <rameem2019@gmail.com>
2023-05-30 16:11:28 +0600

    hadis

---
+++ app/src/main/java/com/example/zakat/hadis_one.java
@@ -0,0 +1,14 @@
+package com.example.zakat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hadis_one extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hadis_one);
+    }
+}
